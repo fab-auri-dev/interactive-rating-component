81d5f4b70d9534a75fcea3a3a95df84cbf8f1337
Fabiano Auri <fabiano.auri@outlook.it>
2023-07-29 08:32:40 +0200

    my solution

---
+++ script.js
@@ -0,0 +1,70 @@
+if ( window.location.pathname == '/' ) {
+    sessionStorage.clear();
+}
+
+const showRating = document.getElementById( 'showRating' );
+const errorText = document.querySelector( '.error' );
+const submitBtn = document.getElementById( 'submitBtn' );
+const ratingBtns = Array.from( document.querySelector( '.rating-section' )?.children ?? [] );
+
+
+function removeSelectedRating( selected ) {
+    errorText.style.display = 'none';
+
+    ratingBtns.forEach( btn => {
+        if ( btn != selected ) {
+            btn.classList.remove( 'active' );
+        };
+    } );
+};
+
+
+function selectedRating() {
+    ratingBtns.forEach( btn => {
+        btn.addEventListener( 'click', () => {
+            btn.classList.toggle( 'active' );
+
+            removeSelectedRating( btn );
+
+            if ( JSON.parse( sessionStorage.getItem( "selectedRating" ) ) == btn.textContent ) {
+                sessionStorage.removeItem( "selectedRating" );
+                
+                return;
+            };
+            sessionStorage.setItem( "selectedRating", JSON.stringify( btn.textContent ) );
+
+        } );
+    } );
+};
+selectedRating();
+
+
+function submitRating() {
+    const getsession = sessionStorage.getItem( "selectedRating" );
+
+    if ( ! getsession ) {
+        errorText.style.display = 'block';
+    } else {
+        window.location.pathname = '/thank-you.html';
+    };
+};
+
+
+if ( ratingBtns.length ) {
+    submitBtn.addEventListener( 'click', submitRating );
+};
+
+
+function showRatingText() {
+    let getsession = sessionStorage.getItem( "selectedRating" );
+
+    if ( showRating ) {
+        showRating.textContent = `You selected ${ JSON.parse( getsession ) } out of 5`;
+
+        setTimeout( () => {
+            window.location.pathname = '/';
+        }, 5000 ); 
+    };
+
+};
+showRatingText();
